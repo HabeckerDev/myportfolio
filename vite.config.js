45046535e22d41286f4e2185e5be5ec7b8149c7f
Dylan Habecker <dchabecker@gmail.com>
2024-07-03 18:14:49 -0700

    add vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/your-repo-name/', // Replace 'your-repo-name' with the name of your GitHub repository
+});
